87d44c400edd86d9dc424933534a91657268edd3
AzizKitten <97047609+AzizKitten@users.noreply.github.com>
2024-03-20 12:31:03 +0100

    Add files via upload

---
+++ assets/js/mods.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            var targetId = this.getAttribute('href').substring(1);
+            var targetSection = document.getElementById(targetId);
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop,
+                    behavior: 'smooth'
+                });
+                navLinks.forEach(function (el) {
+                    el.classList.remove('active');
+                });
+                link.classList.add('active');
+            }
+        });
+    });
+});
+
+function toggleNav() {
+    var navList = document.querySelector('nav ul');
+    navList.style.display = navList.style.display === 'flex' ? 'none' : 'flex';
+}
+
+var currentYear = new Date().getFullYear();
+        document.getElementById('currentYear').textContent = currentYear;
+
+document.querySelectorAll('.button').forEach(item => {
+    item.addEventListener('click', event => {
+      const url = item.getAttribute('data-url');
+      window.open(url, '_blank');
+    });
+  });
+  
